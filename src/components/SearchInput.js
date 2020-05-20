6efc135b0823219de204140bdc3403afe580078f
DuGlaser <duglaser0618@gmail.com>
2020-05-20 23:09:36 +0900

    :art:ADD the SearchInput component style

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,6 +1,40 @@
 import React, { useState } from 'react';
 import { useRecoilState, useSetRecoilState } from 'recoil';
 import { searchState } from '../states/searchState';
+import { css } from 'emotion';
+import { Icon } from '@iconify/react';
+import searchOutlined from '@iconify/icons-ant-design/search-outlined';
+
+const wrapper = css`
+  width: 500px;
+  height: 40px;
+  max-width: 90%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0px 4px 12px rgba(33, 29, 27, 0.12),
+    0px 0px 1px rgba(33, 29, 27, 0.1);
+
+  input {
+    flex: 4;
+    height: 100%;
+    font-size: 14px;
+    padding: 10px 5px;
+    border: none;
+    margin: 0;
+    outline: none;
+  }
+
+  button {
+    height: 100%;
+    font-size: 14px;
+    padding: 10px 10px;
+    border: none;
+    outline: none;
+    box-shadow: 0px 4px 12px rgba(33, 29, 27, 0.12),
+      0px 0px 1px rgba(33, 29, 27, 0.1);
+  }
+`;
 
 const SearchInput = () => {
   const [text, setText] = useState('');
@@ -12,9 +46,11 @@ const SearchInput = () => {
   };
 
   return (
-    <div className="wrapper">
+    <div className={wrapper}>
       <input type="" value={text} onChange={(e) => setText(e.target.value)} />
-      <button onClick={handleSubmit}>検索</button>
+      <button onClick={handleSubmit}>
+        <Icon icon={searchOutlined} style={{ fontSize: '20px' }} />
+      </button>
     </div>
   );
 };
